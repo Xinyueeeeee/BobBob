cc50351b7eccd87423b3a3a3fd7476fc8a8e24ee
Xinyue <ma_xinyue@students.edu.sg>
2025-11-14 10:29:06 +0800

    Initial Commit

---
+++ BobBob/BobBobApp.swift
@@ -0,0 +1,17 @@
+//
+//  BobBobApp.swift
+//  BobBob
+//
+//  Created by Huang Qing on 14/11/25.
+//
+
+import SwiftUI
+
+@main
+struct BobBobApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
