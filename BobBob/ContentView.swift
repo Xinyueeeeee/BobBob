8d3921e5bdfa625544af281f1b7267a5313598de
hanyi2341 <fenghanyi09@gmail.com>
2025-11-14 11:31:58 +0800

    :)

--- BobBob/ContentView.swift
+++ BobBob/ContentView.swift
@@ -10,15 +10,44 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Helluuuu:))))")
+           
         }
-        .padding()
+      
     }
 }
-
-#Preview {
-    ContentView()
+struct IntroductionScreenView : View {
+    let title : String
+    let description : String
+    let imageName : String
+    let isLastPage : Bool
+    
+    
+    var body : some View {
+        Image(imageName)
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+        Text(title)
+            .font(.title3)
+            .fontWeight(.bold)
+            .multilineTextAlignment(.center)
+        Text(description)
+            .font(.body)
+            .multilineTextAlignment(.center)
+            .padding(.horizontal)
+        
+        if isLastPage {
+            Button(action: {
+                
+            }){
+                Text("Get Started!")
+                    .foregroundColor(.white)
+                    .font(.headline)
+            }
+        }
+        
+    }
+    
+    #Preview {
+        ContentView()
+    }
 }
