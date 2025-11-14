0b046eac6365256c2269387143f56363976fb3bb
Dhanvarsini <p.dhanvarsini@gmail.com>
2025-11-14 14:52:16 +0800

    calendar view date

--- BobBob/CalendarView.swift
+++ BobBob/CalendarView.swift
@@ -1,18 +1,12 @@
-//
-//  CalendarView.swift
-//  BobBob
-//
-//  Created by T Krobot on 14/11/25.
-//
 
 import SwiftUI
 
 struct CalendarView: View {
     @State private var color: Color = .blue
     @State private var date = Date.now
-    let daysOfWeek = ["S", "M", "T", "W", "T", "F", "S"]
+    let daysOfWeek = Date.capitalizedFirstLettersOfWeekdays
     let columns = Array(repeating: GridItem(.flexible()), count: 7)
-    
+    @State private var days: [Date] = []
     var body: some View {
         VStack {
             LabeledContent("Calendar Color") {
@@ -22,21 +16,41 @@ struct CalendarView: View {
                 DatePicker("", selection: $date)
             }
             HStack {
-                ForEach(daysOfWeek.indices, id: \.self) {index in
-                    Text(daysOfWeek[index])
+                ForEach(daysOfWeek.indices, id: \.self) { index in
+                Text(daysOfWeek[index])
                         .fontWeight(.black)
                         .foregroundStyle(color)
                         .frame(maxWidth: .infinity)
                 }
-                LazyVGrid(columns: columns, content: {
+            }
+            LazyVGrid(columns: columns) {
+                ForEach(days, id: \.self) { day in
+                    if day.monthInt != date.monthInt {
+                        Text("")
+                    } else {
+                        Text(day.formatted(.dateTime.day()))
+                            .fontWeight(.bold)
+                            .foregroundStyle(.secondary)
+                            .frame(maxWidth: .infinity, minHeight: 40)
+                            .background(
+                                Circle()
+                                    .foregroundStyle(
+                                        Date.now.startOfDay == day.startOfDay
+                                        ? .red.opacity(0.3)
+                                        :  color.opacity(0.3)
+                                    )
+                            )
+                    }
                 }
-                )
-                
-                .padding()
             }
         }
-        
-        
+        .padding()
+        .onAppear {
+            days = date.calendarDisplayDays
+        }
+        .onChange(of: date) {
+            days = date.calendarDisplayDays
+        }
     }
     
 }
