f5d8a82edb1b290a4825d7af70fc4065f6a40117
hanyi2341 <fenghanyi09@gmail.com>
2025-11-14 10:38:49 +0800

    :)

--- BobBob/ContentView.swift
+++ BobBob/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Helluuuu")
+            Text("Helluuuu:))))")
         }
         .padding()
     }
