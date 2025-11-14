bc7e52de2900e9c86498aa9151d014ddd17253b1
hanyi2341 <fenghanyi09@gmail.com>
2025-11-14 16:47:32 +0800

    onboarding

--- BobBob/OnboardingView.swift
+++ BobBob/OnboardingView.swift
@@ -7,18 +7,29 @@
 
 import SwiftUI
 
+
+
 struct OnboardingView: View {
+    
     var body: some View {
-        TabView{
-       
-       //page1
-            IntroductionScreenView(title: "WELCOME", description: "A task organiser that adapts to the way you acually work", imageName: "", isStartingPage: false, isLastPage: false)
-        //page2
-            IntroductionScreenView(title: "", description: "Find your working style", imageName: "", isStartingPage: true, isLastPage: false)
+        ZStack{
             
+            TabView{
+                IntroductionScreenView(title: "WELCOME", description: "A task organiser that adapts to the way you acually work", imageName: "", isStartingPage: false)
+                
+                IntroductionScreenView(title: "", description: "Find your working style", imageName: "", isStartingPage: true)
+                
+                HabitualStyleView()
+                
+                ChronotypeView()
+                
+                NapTimeView()
+                
+            }
+            .tabViewStyle(PageTabViewStyle())
+            .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode: .always))
+           
         }
-        .tabViewStyle(PageTabViewStyle())
-        .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode: .always))
     }
 }
 struct IntroductionScreenView : View {
@@ -26,7 +37,6 @@ struct IntroductionScreenView : View {
     let description : String
     let imageName : String
     let isStartingPage: Bool
-    let isLastPage : Bool
   
     @State private var isSheetPresented = false
     
@@ -57,28 +67,65 @@ struct IntroductionScreenView : View {
                         .shadow(color:Color.black.opacity(0.3),radius: 5, x:0,y:3)
                 }
             }
-            if isLastPage {
-                Button {
-                    
-                } label: {
-                    Text("Get Started!")
-                        .foregroundColor(.white)
-                        .font(.headline)
-                        .padding()
-                        .frame(minWidth:0,maxWidth: .infinity)
-                        .background(Color.blue)
-                        .cornerRadius(20)
-                        .shadow(color:Color.black.opacity(0.3),radius: 5, x:0,y:3)
-                }
-                
-                
+        }
+    }
+}
+
+struct HabitualStyleView : View {
+    var body: some View {
+        VStack{
+            Text("What is your habitual style?")
+                .font( .title)
+            Button{
+            } label: {
+                Text("Hopper")
+                    .font(.title3)
+                Text("Someone who concentrates on a task for short bursts of time")
                 
             }
+            Button{
+            } label: {
+                Text("Hyperfocus")
+                    .font(.title3)
+                Text("Someone who concentrates on a task for a long period of time")
+            }
         }
-        .padding()
-        .fullScreenCover(isPresented: $isSheetPresented, content:{
-            LandingScreen()
-        } )
+    }
+}
+
+struct ChronotypeView : View {
+    var body: some View {
+        VStack{
+            Text("What is your working preference?")
+            Button{
+            } label: {
+                Text("Early Bird")
+            }
+            Button{
+            }label:{
+                Text("Night Owl")
+            }
+        }
+    }
+}
+
+struct NapTimeView: View {
+    @State private var selectedTime = Calendar.current.date(
+        bySettingHour: 8,
+        minute: 0,
+        second: 0,
+        of: Date()
+    )!
+
+    var body: some View {
+        VStack{
+            Text("What time do you sleep?")
+            DatePicker(
+                "Select Time",
+                selection: $selectedTime,
+                displayedComponents: .hourAndMinute
+            )
+        }
     }
 }
 
