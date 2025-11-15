df6a4853cce67bea3bc7585621f91a4fbe26d80f
minyii <minniieeerat@gmail.com>
2025-11-15 10:40:33 +0800

    tab bar

--- BobBob/ContentView.swift
+++ BobBob/ContentView.swift
@@ -9,16 +9,18 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        
         TabView{
-        //eg
-            //IntroductionScreenView(title: <#T##String#>, description: <#T##String#>, imageName: <#T##String#>, isLastPage: <#T##Bool#>)
-                  //page1
-                       //IntroductionScreenView(title: "Welcome", description: "A task organiser that adapts to the way you acually work", imageName: <#T##String#>, isLastPage: false)
-                       
-                       //IntroductionScreenView(title: <#T##String#>, description: <#T##String#>, imageName: <#T##String#>, isLastPage: <#T##Bool#>)
+            Tab("calendar",systemImage: "calendar"){
+                CalendarView()
+            }
+            Tab("tasks",systemImage: "list.bullet"){
+                TasksView(totalSeconds: .constant(0))
+            }
+                    Tab("setttings",systemImage: "gearshape"){
+                        settingsView()
+            }
         }
-        .tabViewStyle(PageTabViewStyle())
-        .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode: .always))
     }
 }
 
